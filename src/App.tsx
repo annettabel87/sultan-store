b132cf5251f125cefdf160f74df08e9bb70d0525
Annettabel <annettabel@rambler.ru>
2023-03-23 23:17:35 +0500

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.module.scss";
+import style from "./App.module.scss";
 import Preloader from "./common/Preloader/Preloader";
 import { ROUTE } from "./common/helpers";
 import { Header } from "./components/Header/Header";
@@ -11,17 +11,24 @@ import Breadcrumbs from "./components/Breadcrumbs/Breadcrumbs";
 
 function App() {
   return (
-    <div className="App">
-       <Header />
-       <Breadcrumbs/>
-      <Suspense fallback={<Preloader />}>
-        <Routes>
-          <Route path={ROUTE.CATALOG} element={<CatalogPage />} />
-          <Route path={ROUTE.CARD} element={<CardPage />} />
-          <Route path={ROUTE.BASKET} element={<BasketPage />} />
-          <Route path={ROUTE.ALL} element={<Navigate to="/catalog" />} />
-        </Routes>
-      </Suspense>
+    <div className={style.App}>
+      <Header />
+      <main className={style.main}>
+        <div className={style.container}>
+          <Breadcrumbs />
+          <Suspense fallback={<Preloader />}>
+            <Routes>
+              <Route path={ROUTE.CATALOG} element={<CatalogPage />} />
+              <Route path={ROUTE.CARD} element={<CardPage />} />
+              <Route path={ROUTE.BASKET} element={<BasketPage />} />
+              <Route
+                path={ROUTE.ALL}
+                element={<Navigate to={ROUTE.CATALOG} />}
+              />
+            </Routes>
+          </Suspense>
+        </div>
+      </main>
     </div>
   );
 }
