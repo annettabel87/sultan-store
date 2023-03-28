8931a991c687b8ecec0270f36f06875d0e01af75
unknown <annettabel@rambler.ru>
2023-03-28 09:24:31 +0300

    add brandblock

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -8,6 +8,11 @@ import { CLEAR_FILTERS } from "../../Redux/catalogReducer";
 import { FilterListBlock } from "../FilterButtonsBlock/FilterListBlock";
 import { ButtonBig } from "../UI/ButtonBig";
 import basket from "../../assets/icon/clearBasket.svg";
+import airWick from "../../assets/img/airW.png";
+import masterFresh from "../../assets/img/masterF.png";
+import sibiar from "../../assets/img/sibiar.png";
+import cotton from "../../assets/img/cotton.png";
+import camay from "../../assets/img/camay.png";
 import style from "./SideBar.module.scss";
 
 export const SideBar: FC = () => {
@@ -33,11 +38,35 @@ export const SideBar: FC = () => {
         <PriceFilter />
         <SearchBlock manufacturers={allManufactures} />
         <div className={style.btnBlock}>
-        <ButtonBig text={"Показать"}/>
+          <ButtonBig text={"Показать"} />
           <button className={style.resetBtn} type="reset" id="reset-button" name="reset-button"><img src={basket} alt="basket" /></button>
         </div>
       </form>
       <FilterListBlock />
+      <span className={style.separator}></span>
+      <div className={style.brandBlock}>
+        <h3 className={style.title}>Бренды</h3>
+        <div className={style.brandWrapp}>
+          <div className={style.brandCard}>
+            <img src={airWick} alt="brand airWick" />
+          </div>
+          <div className={style.brandCard}>
+            <img src={masterFresh} alt="brand masterFresh" />
+          </div>
+          <div className={style.brandCard}>
+            <img src={sibiar} alt="brand sibiar" />
+          </div>
+          <div className={style.brandCard}>
+            <img src={cotton} alt="brand cotton" />
+          </div>
+          <div className={style.brandCard}>
+            <img src={camay} alt="brand camay" />
+          </div>
+        </div>
+        <button className={style.btn}>
+          Показать все
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -37,7 +37,6 @@ export const CatalogPage: FC = () => {
         <SideBar />
         <CardsBlock />
       </div>
-      
     </div>
   );
 };
