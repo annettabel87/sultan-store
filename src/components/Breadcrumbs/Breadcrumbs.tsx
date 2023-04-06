7e3bd201f5d43c21e807b0fbf464520a86e964e9
unknown <annettabel@rambler.ru>
2023-04-06 10:25:56 +0300

    fix breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -16,8 +16,6 @@ export default function Breadcrumbs() {
       return (
         <span key={crumb}>
           <span className={style.crumb} >
-            <NavLink to={ROUTE.CATALOG}>Главная</NavLink>
-            <span className={style.line}></span>
             <NavLink to={currentLink}>{crumb}</NavLink>
           </span>
           <span className={style.line}></span>
